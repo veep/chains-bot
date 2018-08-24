58853d51e69cc664af13e3edad083698a5c0c2f9
Eric Prestemon <ecp@prestemon.com>
2018-08-23 19:35:24 -0700

    Successfully post the first seed
    Include title/author/image
    No state updating, no support for chains yet.

--- bookchains.pl
+++ bookchains.pl
@@ -3,9 +3,12 @@
 use common::sense;
 use FindBin;
 use lib "$FindBin::Bin/cpan/lib/perl5";
-use IO::All qw/io/;
+
 use File::Basename qw/fileparse/;
+use File::Temp qw/ tempfile /;
+use IO::All qw/io/;
 use JSON::PP;
+use Mastodon::Client;
 
 my $DEBUG = 1;
 
@@ -27,8 +30,8 @@ sub main {
         }
     } until item_validates($next_item);
 
-    post_item($state);
-    save_state($state);
+    post_item($state, $next_item);
+    save_state($state, $next_item);
 }
 
 
@@ -68,15 +71,24 @@ sub validated_seed {
         my $seed_id = shift @{$state->{unused_seeds}};
         push @{$state->{used_seeds}}, $seed_id;
         my $results < io('http://openlibrary.org/search.json?q=' . $seed_id);
-        DEBUG('http get',$results);
+#        DEBUG('http get',$results);
         my $parsed_results = decode_json $results;
         if ($parsed_results->{num_found} > 0) {
             my $seed = cleanup_item($parsed_results->{docs}[0]);
-            DEBUG('clean object', $seed);
-            return $seed;
+            $seed = mark_as_seed($seed);
+#            DEBUG('clean object', $seed);
+            if (item_validates($seed)) {
+                return $seed;
+            }
         }
     }
     die "There are no unused seeds, and it's seed time\n";
+}
+
+sub mark_as_seed {
+    my $item = shift;
+    $item->{is_seed} = 1;
+    return $item;
 }
 
 sub cleanup_item {
@@ -99,12 +111,69 @@ sub cleanup_item {
 
 
 sub next_from_state {}
-sub item_validates { return 1;}
-sub post_item {}
+sub item_validates {
+    my $item = shift;
+    if (! exists $item->{cover_edition_key}) {
+	return 0;
+      }
+    if (! exists $item->{cover_image}) {
+      my $url = 	'http://covers.openlibrary.org/b/olid/'
+	. $item->{cover_edition_key}
+	. '-M.jpg';
+      DEBUG('cover image',$url);
+      my $image < io($url );
+      DEBUG('image length',length($image));
+      $item->{cover_image} = $image;
+    }
+    if (length($item->{cover_image}) < 1000) {
+      return 0;
+    }
+    DEBUG("validated");
+    return 1;
+}
+sub post_item {
+  my ($state, $item) = @_;
+  my $filename = "./tempfile";
+  $item->{cover_image} > io($filename);
+
+  my $secrets = decode_json scalar io->file('secrets.json')->slurp;
+  my $client_id = $secrets->{client_id};
+  my $client_secret = $secrets->{client_secret};
+  my $access_token = $secrets->{access_token};
+
+  my $client = Mastodon::Client->new
+    (
+     instance        => 'botsin.space',
+     name            => 'BookChains',
+     client_id       => $client_id,
+     client_secret   => $client_secret,
+     access_token    => $access_token,
+     coerce_entities => 0,
+    );
+  my $media_id= $client->upload_media($filename);
+  DEBUG('media_id',$media_id);
+  my $text;
+  if ($item->{is_seed}) {
+    $text = "Let's start a new chain with '" .
+      ($item->{title_suggest} // $item->{title}) . "'\n";
+    $text .= 'by ' . join(' & ',@{$item->{author_name}}) . "\n";
+    if ($text && $media_id->{id}) {
+      my $status = $client->post_status
+	(
+	 $text,
+	 {
+	  visibility => 'unlisted',
+	  media_ids => [$media_id->{id}],
+	 }
+	);
+      DEBUG('return status',$status);
+    }
+  }
+}
 
 sub DEBUG {
-    use Data::Dump;
+    use Data::Dump qw/dd/;
     if ($DEBUG) {
-        ddx [@_];
+        dd [@_];
     }
 }
